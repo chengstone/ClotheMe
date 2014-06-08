e4bf8598d0676e23889402cf8cc6cbb03b1f2482
chengstone <69558140@163.com>
2014-06-08 21:08:29 +0800

    分类存档info类初步完成
    数据库加密和NDK方式暂缓，等到应用可以发布的时候再改来得及。
    将分类存档info类写完，其他的info类都可以横展开了，可以先写到LOAD方法即可，其他方法等到业务逻辑时现用现写也来得及。

--- ClotheMe/src/com/logicalModelLayer/clotheme/CategoryArchiveInfo.java
+++ ClotheMe/src/com/logicalModelLayer/clotheme/CategoryArchiveInfo.java
@@ -1,6 +1,7 @@
 package com.logicalModelLayer.clotheme;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import android.content.Context;
@@ -10,7 +11,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.util.SparseArray;
 
 import com.common.clothe.*;
+import com.daogen.clotheme.CategoryArchive;
 import com.daogen.clotheme.CategoryArchiveDao;
+import com.daogen.clotheme.CategoryArchiveDao.Properties;
+
+import de.greenrobot.dao.query.DeleteQuery;
+import de.greenrobot.dao.query.QueryBuilder;
 /*
  * @brief 分类存档表类
  * */
@@ -18,25 +24,26 @@ public class CategoryArchiveInfo {
 	/*
 	 * @brief 分类存档结构
 	 * */
-	public class CategoryArchiveStruct {
-//		int meterialID;					//物品ID
-		public boolean isWashRemind;	//是否清洗提醒
-		public String remindTime;		//提醒时间
-		public String remindFrequency;	//提醒频率
-		//CategoryArchiveStruct(){}
-	}
+//	public class CategoryArchiveStruct {
+////		int meterialID;					//物品ID
+//		public boolean isWashRemind;	//是否清洗提醒
+//		public String remindTime;		//提醒时间
+//		public String remindFrequency;	//提醒频率
+//		//CategoryArchiveStruct(){}
+//	}
 	/*
 	 * @brief DB表名
 	 * */
 	private final String TABLENAME = "categoryarchive";
 	// Context of application  
     private Context context = null;  
-	private SQLiteDatabase m_db;
+	private SQLiteDatabase m_db = null;
 	private static CategoryArchiveInfo m_instance = null;  
 	/*
 	 * @brief 分类存档表map
 	 * */
-	private Map<Integer,CategoryArchiveStruct> m_CategoryArchiveData = null;
+//	private Map<Integer,CategoryArchiveStruct> m_CategoryArchiveData = null;
+	private Map<Integer,CategoryArchive> m_CategoryArchiveData = null;
 	private CategoryArchiveDao m_CategoryArchiveDao = null;
 	/*
 	 * @brief  分类存档表类构造函数
@@ -44,11 +51,12 @@ public class CategoryArchiveInfo {
 	 * @return 无
 	 * */
     private CategoryArchiveInfo(Context context) {  	
-    	m_CategoryArchiveData = new HashMap<Integer,CategoryArchiveStruct>();
+//    	m_CategoryArchiveData = new HashMap<Integer,CategoryArchiveStruct>();
+    	m_CategoryArchiveData = new HashMap<Integer,CategoryArchive>();
     	this.context = context;
 //    	m_db = AssetsDatabaseManager.getInstance(context).getDatabase();
     	m_CategoryArchiveDao = AssetsDatabaseManager.getInstance(context).getCategoryArchiveDao();
-//    	Load();
+    	Load();
     }  
     /*
      * @brief  获取本类单例
@@ -68,23 +76,30 @@ public class CategoryArchiveInfo {
     public int Load(){
     	int ret = CommonDefine.NO_ERROR;
     	//Cursor cursor = m_db.query(TABLENAME, new String[]{"personid,name,age"}, "name like ?", new String[]{"%传智%"}, null, null, "personid desc", "1,2");
-    	Cursor cursor = m_db.rawQuery("select * from " + TABLENAME, null);
-    	while (cursor.moveToNext()) {
-    		CategoryArchiveStruct cas = new CategoryArchiveStruct();
-    		int meterialID;
-    		meterialID = cursor.getInt(1);
-    		cas.isWashRemind = cursor.getInt(2) == 0?false:true; 
-    		cas.remindFrequency = cursor.getString(4);
-    		cas.remindTime = cursor.getString(3);
-    		m_CategoryArchiveData.put(meterialID, cas);
-    		if(CommonDefine.isDebug){
-        		Log.d("meterialID = " + String.valueOf(meterialID));
-        		Log.d("isWashRemind = " + String.valueOf(cursor.getInt(2)));
-        		Log.d("remindFrequency = " + cas.remindFrequency);
-        		Log.d("remindTime = " + cas.remindTime);
-    		}
-    	}
-    	cursor.close();
+//    	Cursor cursor = m_db.rawQuery("select * from " + TABLENAME, null);
+//    	while (cursor.moveToNext()) {
+//    		CategoryArchiveStruct cas = new CategoryArchiveStruct();
+//    		int meterialID;
+//    		meterialID = cursor.getInt(1);
+//    		cas.isWashRemind = cursor.getInt(2) == 0?false:true; 
+//    		cas.remindFrequency = cursor.getString(4);
+//    		cas.remindTime = cursor.getString(3);
+//    		m_CategoryArchiveData.put(meterialID, cas);
+//    		if(CommonDefine.isDebug){
+//        		Log.d("meterialID = " + String.valueOf(meterialID));
+//        		Log.d("isWashRemind = " + String.valueOf(cursor.getInt(2)));
+//        		Log.d("remindFrequency = " + cas.remindFrequency);
+//        		Log.d("remindTime = " + cas.remindTime);
+//    		}
+//    	}
+//    	cursor.close();
+    	List<CategoryArchive> ca = m_CategoryArchiveDao.loadAll();
+    	for(int i = 0; i < ca.size(); i++)  
+        {  
+    		CategoryArchive cas = new CategoryArchive();
+            cas = ca.get(i);  
+            m_CategoryArchiveData.put(cas.getMeterialID(), cas);
+        }  
     	return ret;
     }
     /* 查
@@ -93,11 +108,12 @@ public class CategoryArchiveInfo {
      * @param  out_CategoryArchiveStruct value将保存其中
      * @return 成功 NO_ERROR 失败 SYSTEM_ERROR
      * */
-    public int getCategoryArchiveStruct(int in_meterialID,CategoryArchiveStruct out_CategoryArchiveStruct){
+//    public int getCategoryArchiveStruct(int in_meterialID,CategoryArchiveStruct out_CategoryArchiveStruct){
+    public int getCategoryArchive(int in_meterialID,CategoryArchive out_CategoryArchive){
     	if(m_CategoryArchiveData == null
-    	|| out_CategoryArchiveStruct == null
+    	|| out_CategoryArchive == null
     	|| in_meterialID <= 0){
-        	Log.w("return SYSTEM_ERROR");
+        	Log.w("Param invalid, return SYSTEM_ERROR");
     		return CommonDefine.SYSTEM_ERROR;
     	}
     	if(m_CategoryArchiveData.isEmpty() == true){
@@ -105,9 +121,9 @@ public class CategoryArchiveInfo {
     		return CommonDefine.SYSTEM_ERROR;
     	}
     	if(m_CategoryArchiveData.containsKey(in_meterialID) == true){
-    		out_CategoryArchiveStruct = m_CategoryArchiveData.get(in_meterialID);
-    		if(out_CategoryArchiveStruct == null){
-            	Log.w("get failed");
+    		out_CategoryArchive = m_CategoryArchiveData.get(in_meterialID);
+    		if(out_CategoryArchive == null){
+            	Log.w("CategoryArchive get failed");
         		return CommonDefine.SYSTEM_ERROR;
     		}
     	}
@@ -123,28 +139,33 @@ public class CategoryArchiveInfo {
      * @param  in_CategoryArchiveStruct 将要写入map的value
      * @return 成功 NO_ERROR 失败 SYSTEM_ERROR
      * */
-    public int setCategoryArchiveStruct(int in_meterialID,CategoryArchiveStruct in_CategoryArchiveStruct){
+    public int setCategoryArchive(int in_meterialID,CategoryArchive in_CategoryArchive){
     	if(m_CategoryArchiveData == null
-    	|| in_CategoryArchiveStruct == null
+    	|| in_CategoryArchive == null
     	|| in_meterialID <= 0){
-        	Log.w("return SYSTEM_ERROR");
+        	Log.w("Param invalid,return SYSTEM_ERROR");
     		return CommonDefine.SYSTEM_ERROR;
     	}
 
     	if(m_CategoryArchiveData.containsKey(in_meterialID) == true){
         	Log.w("meterialID " + String.valueOf(in_meterialID) + " is already exsists");
     		if(m_CategoryArchiveData.remove(in_meterialID) == null){
-            	Log.w("remove failed");
+            	Log.w("CategoryArchive remove failed");
         		return CommonDefine.SYSTEM_ERROR;
     		}
+    		QueryBuilder<CategoryArchive> qb = m_CategoryArchiveDao.queryBuilder();
+    		DeleteQuery<CategoryArchive> bd = qb.where(Properties.MeterialID.eq(in_meterialID)).buildDelete();
+    		bd.executeDeleteWithoutDetachingEntities();
 //    		return CommonDefine.SYSTEM_ERROR;
     	}
 
-    	if(m_CategoryArchiveData.put(in_meterialID,in_CategoryArchiveStruct) == null){
-        	Log.w("put failed");
+    	if(m_CategoryArchiveData.put(in_meterialID,in_CategoryArchive) == null){
+        	Log.w("CategoryArchive put failed");
     		return CommonDefine.SYSTEM_ERROR;
     	}
 
+    	m_CategoryArchiveDao.insert(in_CategoryArchive);
+    	
     	return CommonDefine.NO_ERROR;
     }
     /* 删
@@ -152,10 +173,10 @@ public class CategoryArchiveInfo {
      * @param  in_meterialID 指定key
      * @return 成功 NO_ERROR 失败 SYSTEM_ERROR
      * */
-    public int removeCategoryArchiveStruct(int in_meterialID){
+    public int removeCategoryArchive(int in_meterialID){
     	if(m_CategoryArchiveData == null
     	|| in_meterialID <= 0){
-        	Log.w("return SYSTEM_ERROR");
+        	Log.w("param invalid, return SYSTEM_ERROR");
     		return CommonDefine.SYSTEM_ERROR;
     	}
     	if(m_CategoryArchiveData.isEmpty() == true){
@@ -167,6 +188,9 @@ public class CategoryArchiveInfo {
             	Log.w("remove failed");
         		return CommonDefine.SYSTEM_ERROR;
     		}
+    		QueryBuilder<CategoryArchive> qb = m_CategoryArchiveDao.queryBuilder();
+    		DeleteQuery<CategoryArchive> bd = qb.where(Properties.MeterialID.eq(in_meterialID)).buildDelete();
+    		bd.executeDeleteWithoutDetachingEntities();
     	}
     	else {
         	Log.w("m_CategoryArchiveData does not contains key " + String.valueOf(in_meterialID));
